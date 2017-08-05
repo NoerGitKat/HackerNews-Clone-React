919bfafd15bdc2a6b996d2c77a34ca8b5162276b
Noer <noerpaanakker@hotmail.com>
2017-08-05 22:08:13 +0200

    started to use the Hacker News API, removed artificial data

--- src/App.js
+++ src/App.js
@@ -4,24 +4,31 @@ import './App.css';
 // import Table from './Table.js';
 // import Button from './Button.js';
 
-const list = [
-          {
-          title: 'React',
-          url: 'https://facebook.github.io/react/',
-          author: 'Jordan Walke',
-          num_comments: 3,
-          points: 4,
-          objectID: 0,
-          },
-          {
-          title: 'Redux',
-          url: 'https://github.com/reactjs/redux',
-          author: 'Dan Abramov, Andrew Clark',
-          num_comments: 2,
-          points: 5,
-          objectID: 1,
-          },
-];
+//default values
+const DEFAULT_QUERY = 'redux';
+
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
+
+// const list = [
+//           {
+//           title: 'React',
+//           url: 'https://facebook.github.io/react/',
+//           author: 'Jordan Walke',
+//           num_comments: 3,
+//           points: 4,
+//           objectID: 0,
+//           },
+//           {
+//           title: 'Redux',
+//           url: 'https://github.com/reactjs/redux',
+//           author: 'Dan Abramov, Andrew Clark',
+//           num_comments: 2,
+//           points: 5,
+//           objectID: 1,
+//           },
+// ];
 
 function isSearched(searchTerm) {
   return function(item) {
@@ -35,23 +42,43 @@ class App extends Component {
     super(props);
 
     this.state = {
-      list: list,
-      searchTerm: '',
+      result: null,
+      searchTerm: DEFAULT_QUERY
     };
 
+
+    this.setSearchTopstories = this.setSearchTopstories.bind(this);
+    this.fetchSearchTopstories = this.fetchSearchTopstories.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
-  }
+  };
+
+  setSearchTopstories(result) {
+    this.setState({ result });
+  };
+
+  fetchSearchTopstories(searchTerm) {
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+      .then(response => response.json())
+      .then(result => this.setSearchTopstories(result))
+      .catch(error => console.log(`Beep boop an error has occurred: ${error}`))
+  };
+
+  //lifecycle method
+  componentDidMount() {
+    const { searchTerm } = this.state;
+    this.fetchSearchTopstories(searchTerm);
+  };
 
   onDismiss(id) {
     const isNotId = item => item.objectID !== id;
     const updatedList = this.state.list.filter(isNotId);
     this.setState({ list: updatedList });
-  }
+  };
 
   onSearchChange(event) {
     this.setState({ searchTerm: event.target.value })
-  }
+  };
 
   render() {
     const { searchTerm, list } = this.state;
